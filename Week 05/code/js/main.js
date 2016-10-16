c197aa94378fd17c64f9ab9d227f9b8b98a53608
Jen Doyle <jendoyledesign@gmail.com>
2016-10-15 22:06:28 -0400

    Week 05 HW

--- Week 05/code/js/main.js
+++ Week 05/code/js/main.js
@@ -1,44 +1,24 @@
-$(document).ready(function(){
+$(document).ready(function() {
+  $('#submit-btn').on("click", function(evt){ // Create an on click event for the submit button
+  evt.preventDefault();  // Prevent a form submission
+    var city = $('#city-type').val(); // Store user input
+    $('#city-input').val(''); // Get the string value of your user's input
+       console.log(city); 
 
-	console.log ("here");
+    // Assign the city variable the value of the city input field
+    // Get the first element from an attribute name using
 
-// Write a function that updates the background image of the page based off the city input field
-
-// Define a variable named city
-
-var city;
-
-console.log("city");
-
-// Create an on click event for the submit button
-
-$("#submit-btn").on("click", function (evt){
-
-
-		// Prevent a form submission
-
-	evt.preventDefault();
-	changeCityImage ();
-
-	var city = $("#city-type").val();
-
-	$("#city-type").text(city);
-
-
-	});
-
-// Define changeCityImage function
-
-function changeCityImage (){
-
-	// Assign the city variable the value of the city input field
-
-
-		if (city = "New York" || "New York City" || "NYC") {
-				$("body").css("background", "url('images/nyc.jpg')" );
-			} 
-  
-	};
-
-
+    if (city == "NYC" || city =="nyc" || city == "New York" || city == "New York City" || city == "new york city"){ // How do I do other options: ny, New york, etc?
+      $('body').attr('class','nyc');
+    } else if (city == "SF" || city == "sf" || city =="san fran" || city == "San Fran" || city == "San Francisco") {      
+       $('body').attr('class','sf');
+    } else if (city == "LA" || city =="los angeles" || city == "Los Angeles" || city == "LAX") {      
+       $('body').attr('class','la');
+    } else if (city == "AUS" || city =="austin" || city == "Austin" || city == "Austin Texas" || city == "ATX") {      
+       $('body').attr('class','austin');
+    } else if (city == "SYD" || city =="sydney" || city == "Sydney") {      
+       $('body').attr('class','sydney');
+    }
+  });
 });
+
